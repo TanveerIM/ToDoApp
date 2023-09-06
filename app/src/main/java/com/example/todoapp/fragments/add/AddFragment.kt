266513f3ed45a62c297fba26bb12d40a058b7e43
TanveerMM <tanveeratworks@gmail.com>
2023-09-07 00:03:45 +0530

    creating UI layout and adding binding fragments

--- app/src/main/java/com/example/todoapp/fragments/add/AddFragment.kt
+++ app/src/main/java/com/example/todoapp/fragments/add/AddFragment.kt
@@ -6,15 +6,26 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.todoapp.R
+import com.example.todoapp.databinding.FragmentAddBinding
 
 class AddFragment : Fragment() {
 
+    private var _binding: FragmentAddBinding? = null
+    private val binding get() = _binding!!
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add, container, false)
+        //return inflater.inflate(R.layout.fragment_add, container, false)
+        _binding = FragmentAddBinding.inflate(layoutInflater, container, false)
+
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
 }
